3b6e41e4a6857ec054f04da3cc785b69ad04f729
zhenyuan <936451076@qq.com>
2020-07-22 10:18:11 +0800

    fix：视图层改变

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -3,6 +3,9 @@ from apps.admin import admin
 from apps.admin.forms import AdminForm
 from apps.admin.minddleware.middleware import admin_login_required
 
+from apps import socketio
+from apps.utils.command import ps_command, top_command, tail_command
+
 
 @admin.route('/login', methods=['POST', 'GET'])
 def login():
@@ -20,3 +23,41 @@ def login():
 @admin_login_required
 def index():
     return render_template('admin/index.html')
+
+
+@socketio.on('connect', namespace="/shell")
+def connect():
+    print("connect..")
+
+
+@socketio.on('disconnect', namespace="/shell")
+def disconnect():
+    print("disconnect..")
+
+
+@socketio.on('client', namespace="/shell")
+def client_info(data):
+    print('client data', data)
+    _type = data.get('_type')
+    if _type == 'tail':
+        tail_command.background_thread()
+    elif _type == 'ps':
+        ps_command.background_thread()
+    elif _type == 'top':
+        top_command.background_thread()
+    else:
+        socketio.emit('response', {'text': '未知命令'}, namespace='/shell')
+
+
+@socketio.on('leave', namespace="/shell")
+def leave(data):
+    print('leave data', data)
+    _type = data.get('_type')
+    if _type == 'tail':
+        tail_command.leave()
+    elif _type == 'ps':
+        ps_command.leave()
+    elif _type == 'top':
+        top_command.leave()
+    else:
+        socketio.emit('response', {'text': '未知命令'}, namespace='/shell')
